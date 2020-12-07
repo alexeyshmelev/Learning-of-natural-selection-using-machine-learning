0edc3235fb65506027658479cba77eb11e4819c5
Alexey Shmelev <60858323+Grenlex@users.noreply.github.com>
2020-12-07 13:35:06 +0300

    Update selam_rnn.py

--- selam_rnn.py
+++ selam_rnn.py
@@ -1,45 +1,8 @@
-# Lab 12 RNN
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-
-#x_data = [5.6, 4.5, 7.8, 5.7, 3.4, 4.9, 6.2, 4.8, 5.1, 5.5, 19.2, 22.7, 17.7, 16.5, 25.1, 23.8, 19.9, 18.1, 23.3, 17.9]
-#y_data = [1, 0]
-#x_data = [[[i * 10 for i in x_data]]]
-
-x_data = []
-y_data = []
-
-for i in [0, 1]:
-  needed_line = ''
-  file_line = 'frequency' + str(i) + '.txt'
-  file = open(file_line, 'r')
-
-  for line in file:
-    array = line.split('\t')
-    x_data += [float(array[2])]
-
-  file.close()
-
-  file_line = 'example' + str(i) + '_selection.txt'
-  file = open(file_line, 'r')
-  line = file.read()
-  file.close()
-  array = line.split('\t')
-  y_data += [float(array[3])*10]
-
-y_data = [0, 1]
-
-
-# As we have one batch of samples, we will change them to variables only once
-inputs = Variable(torch.tensor(x_data, requires_grad=True))
-inputs = inputs.view(1, 2, -1)
-print(inputs)
-labels = Variable(torch.LongTensor(y_data))
-print(labels)
-
-
-
+import torch.utils.data as data_utils
+from torch.utils.data import TensorDataset, DataLoader
 
 num_classes = 2
 input_size = 20
@@ -48,53 +11,85 @@ batch_size = 1
 sequence_length = 20 
 num_layers = 1  # one-layer rnn
 
+def FormData(train_number, class_number):
+
+  data = []
+  labels = []
+
+  for i in range(train_number):
+    for j in range(0, class_number):
+      file_line = 'selection_' + str(j) + '_frequency_' + str(i) + '.txt'
+      file = open(file_line, 'r')
+      for line in file:
+        array = line.split('\t')
+        data += [float(array[2])]
+      file.close()
+  
+  for i in range(class_number):
+    labels.append(i)
+  
+  labels = Variable(torch.LongTensor(labels)) #сразу выводим labels в нужном формате
+
+  return data, labels
+      
+
+
+class SampleDataset(Dataset):
+  def __init__(self, secuence):
+    self.samples=secuence
+ 
+  def __len__(self):
+      return len(self.samples)
+ 
+  def __getitem__(self,idx):
+      return (self.samples[idx])
+
 
 class Model(nn.Module):
 
-    def __init__(self, num_layers, hidden_size):
-        super(Model, self).__init__() #вызываем конструктор класса nn.Module через метод super(), здесь показан пример для версии Python 2.0
-        self.num_layers = num_layers #количество скрытых слоёв RNN
-        self.hidden_size = hidden_size #сколько скрытых состояний
-        self.rnn = nn.RNN(input_size=20,
-                          hidden_size=20, batch_first=True, bidirectional=True)
-        self.fc = nn.Linear(40, num_classes)
+  def __init__(self, num_layers, hidden_size):
+    super(Model, self).__init__() #вызываем конструктор класса nn.Module через метод super(), здесь показан пример для версии Python 2.0
+    self.num_layers = num_layers #количество скрытых слоёв RNN
+    self.hidden_size = hidden_size #сколько скрытых состояний
+    self.rnn = nn.RNN(input_size=20, hidden_size=20, batch_first=True, bidirectional=True)
+    self.fc = nn.Linear(40, num_classes)
+    self.relu=nn.ReLU()
 
-    def forward(self, x):
-        # Initialize hidden and cell states
-        # (num_layers * num_directions, batch, hidden_size)
-        h_0 = Variable(torch.zeros(
-            self.num_layers, x.size(0), 40))
-        h_0 = h_0.view(2, -1, 20)
-        #print("h_0:",h_0)
+  def forward(self, x): # Initialize hidden and cell states (num_layers * num_directions, batch, hidden_size)
+    h_0 = Variable(torch.zeros(self.num_layers, x.size(0), 40))
+    h_0 = h_0.view(2, -1, 20)
+    #print("h_0:",h_0)
 
-        x_m = x.view(batch_size, -1, sequence_length)
-        #print("x_m:",x_m)
-        # Propagate embedding through RNN
-        # Input: (batch, seq_len, embedding_size)
-        # h_0: (num_layers * num_directions, batch, hidden_size)
-        out, _ = self.rnn(x_m, h_0)
-        #print("out:", out.view(-1, 40))
-        return self.fc(out.view(-1, 40))
+    x_m = x.view(batch_size, -1, sequence_length) # h_0: (num_layers * num_directions, batch, hidden_size)
+    #print("x_m:",x_m)
+    out, _ = self.rnn(x_m, h_0)
+    out = self.relu(out)
+    out = self.fc(out.view(-1, 40))
+    #print("out:", out.view(-1, 40))
+    return self.relu(out)
 
-
-# Instantiate RNN model
+# Initialize model, set loss and optimizer function, CrossEntropyLoss = LogSoftmax + NLLLoss
 model = Model(num_layers, hidden_size)
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 print(model)
 
-# Set loss and optimizer function
-# CrossEntropyLoss = LogSoftmax + NLLLoss
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+data, labels = FormData(1, 2)
+print(data, labels)
+dataset = DataLoader(data, batch_size=40, shuffle=False, num_workers=2)
 
 # Train the model
-for epoch in range(100):
+for i, batch in enumerate(dataset):
+  inputs = batch.clone().detach().requires_grad_(True).float()
+  inputs = inputs.view(1, 2, -1)
+  print(inputs)
+  for epoch in range(100):
     outputs = model(inputs)
     #print(outputs)
     optimizer.zero_grad()
     loss = criterion(outputs, labels)
     loss.backward()
     optimizer.step()
-    _, idx = outputs.max(1)
    
     print("epoch: %d, loss: %1.3f" % (epoch + 1, loss.item()))
 

a46f7acb453f41c41cd2bb0be26e1e9e6f855b44
Alexey Shmelev <60858323+Grenlex@users.noreply.github.com>
2021-03-12 14:08:19 +0300

    Update BioPack.py

--- BioPack.py
+++ BioPack.py
@@ -1,8 +1,9 @@
 import sys
+import matplotlib.pyplot as plt
 import requests
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QPushButton, QMainWindow, QFileDialog, QStackedLayout, QAction, QMessageBox
-from PyQt5.QtGui import QIcon, QPalette, QColor, QLinearGradient, QBrush, QFont, QDesktopServices
+from PyQt5.QtGui import QIcon, QPalette, QColor, QLinearGradient, QBrush, QFont, QDesktopServices, QPixmap
 from PyQt5.QtCore import QUrl
 import torch
 import numpy as np
@@ -35,6 +36,48 @@ class SLS(nn.Module):
         return out
 
 
+class GEN(nn.Module):
+
+    def __init__(self, gen_num_classes):
+        super(GEN, self).__init__()
+        encoder_layers = nn.TransformerEncoderLayer(d_model=gen_num_classes*5, nhead=5, dim_feedforward=2048, dropout=0.1)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers=1)
+        self.decoder = nn.Linear(gen_num_classes*5, 1)
+
+    def forward(self, x):
+        output = self.transformer_encoder(x)
+        output = self.decoder(output).view(1, 1)
+        return output
+
+
+class FRC(nn.Module):
+
+    def __init__(self, gen_num_classes):
+        super(FRC, self).__init__()
+        encoder_layers = nn.TransformerEncoderLayer(d_model=gen_num_classes*5, nhead=5, dim_feedforward=10000, dropout=0)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers=1)
+        self.decoder = nn.Linear(gen_num_classes*5, 1)
+
+    def forward(self, x):
+        output = self.transformer_encoder(x)
+        output = self.decoder(output).view(1, 1)
+        return output
+
+
+class ADM(nn.Module):
+
+    def __init__(self, gen_num_classes):
+        super(ADM, self).__init__()
+        encoder_layers = nn.TransformerEncoderLayer(d_model=gen_num_classes*5, nhead=5, dim_feedforward=10000, dropout=0)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers=1)
+        self.decoder = nn.Linear(gen_num_classes*5, 1)
+
+    def forward(self, x):
+        output = self.transformer_encoder(x)
+        output = self.decoder(output).view(1, 1)
+        return output
+
+
 class App(QMainWindow):
 
     def __init__(self):
@@ -45,7 +88,7 @@ class App(QMainWindow):
 
     def initui(self):
 
-        self.resize(720, 460)
+        self.resize(1200, 900)
         self.setWindowTitle('BioPack')
         self.setWindowIcon(QIcon('BioPack.png'))
         stackedLayout = QStackedLayout()
@@ -93,7 +136,7 @@ class App(QMainWindow):
         initial_widget = QWidget()
         initial_widget.setAutoFillBackground(True)
         palette = initial_widget.palette()
-        gradient = QLinearGradient(-100, 600, 1200, -100)
+        gradient = QLinearGradient(-100, 1000, 1800, -100)
         gradient.setColorAt(0.0, QColor(0, 150, 115))
         gradient.setColorAt(1.0, QColor(255, 255, 255))
         palette.setBrush(QPalette.Window, QBrush(gradient))
@@ -111,21 +154,32 @@ class App(QMainWindow):
             widget = QWidget()
             widget.setAutoFillBackground(True)
             palette = widget.palette()
-            gradient = QLinearGradient(-100, 600, 1200, -100)
+            gradient = QLinearGradient(-100, 1000, 1800, -100)
             gradient.setColorAt(0.0, QColor(0, 150, 115))
             gradient.setColorAt(1.0, QColor(255, 255, 255))
             palette.setBrush(QPalette.Window, QBrush(gradient))
             widget.setPalette(palette)
             page2 = QGridLayout(widget)
+
             result = self.analyze()
+
             title_result = QLabel('Results:')
             title_result.setFont(QFont("San-Serif", 17, QFont.Bold))
             title_result.setStyleSheet('''color: rgb(255, 255, 255);''')
+
             result = QLabel(result)
             result.setFont(QFont("San-Serif", 12))
             result.setStyleSheet('''color: rgb(255, 255, 255);''')
             page2.addWidget(title_result, 0, 0, QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom)
             page2.addWidget(result, 1, 0, QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
+
+            pixmap = QPixmap("likelihood.png")
+            pixmap = pixmap.scaled(700, 700, QtCore.Qt.KeepAspectRatio)
+            lbl = QLabel()
+            lbl.setPixmap(pixmap)
+            lbl.setStyleSheet('''padding-bottom: 30px;''')
+            page2.addWidget(lbl, 2, 0, QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
+
             self.setCentralWidget(widget)
 
         if self.step == 1:
@@ -142,6 +196,7 @@ class App(QMainWindow):
     def analyze(self):
 
         device = torch.device('cpu')
+
         model_sls = SLS(1, 1, 1, 100, 100)  # определяем класс с помощью уже натренированной сети
         model_sls.load_state_dict(torch.load('rnn_v_1_0_0.pth', map_location=device))
         model_sls.eval()
@@ -151,12 +206,46 @@ class App(QMainWindow):
         for line in self.file:
             array = line.split('\t')
             test_data += [float(array[2])]
-        self.file.close()
         test_data = torch.tensor(test_data)
         test_outputs = model_sls(test_data)
         test_outputs = m(test_outputs).cpu().detach().numpy().flatten()
         max = np.argmax(test_outputs)
-        result = 'Natural selection was in locus {:.2} with the probability of {}'.format(max/100, test_outputs[max])
+        result_locus = 'Natural selection was in locus {:.2} with the probability of {:.2}'.format(max/100, test_outputs[max])
+
+        positions = [round(i/100, 2) for i in range(100)]
+        plt.clf()
+        plt.plot(positions, test_outputs, '.-g', label='Likelihood along all genome', alpha=0.5)
+        plt.legend(loc='best')
+        plt.savefig('likelihood.png')
+
+        model_gen = GEN(100)  # определяем класс с помощью уже натренированной сети
+        model_gen.load_state_dict(torch.load('transformer_gen_v_1_0_0.pth', map_location=device))
+        model_gen.eval()
+
+        self.file.seek(0)
+        test_data = []
+        for line in self.file:
+            array = line.split('\t')
+            test_data += [float(array[2]), float(array[3]), float(array[4]), float(array[5]), float(array[6])]
+        test_data = torch.tensor(test_data).float().view(1, 1, 100 * 5)
+        test_outputs = model_gen(test_data).cpu().detach().numpy().flatten().item()
+        result_gen = 'We are talking about {}th generation'.format(round(test_outputs))
+
+        model_frc = FRC(100)  # определяем класс с помощью уже натренированной сети
+        model_frc.load_state_dict(torch.load('transformer_force_v_1_0_0.pth', map_location=device))
+        model_frc.eval()
+
+        test_outputs = model_frc(test_data).cpu().detach().numpy().flatten().item()
+        result_frc = 'The force of natural selection was {}'.format(round(test_outputs/100, 2))
+
+        model_adm = ADM(100)  # определяем класс с помощью уже натренированной сети
+        model_adm.load_state_dict(torch.load('transformer_adm_v_1_0_0.pth', map_location=device))
+        model_adm.eval()
+
+        test_outputs = model_adm(test_data).cpu().detach().numpy().flatten().item()
+        result_adm = 'The admixture on the first stage was {}'.format(round(test_outputs / 100, 2))
+
+        result = result_locus + '\n' + result_gen + '\n' + result_frc + '\n' + result_adm
 
         return result
 

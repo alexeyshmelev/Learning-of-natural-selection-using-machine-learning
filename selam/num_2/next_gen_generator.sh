c2959c56349be0e72bb2b1e023665e58f6593238
Alexey Shmelev <60858323+alexeyshmelev@users.noreply.github.com>
2021-11-17 22:47:46 +0300

    Delete selam directory

--- selam/num_2/next_gen_generator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SELAM -d demography.txt -o output.txt -s selection.txt -h -c 1 0.01
-SELAM_STATS -i population_output.txt -a 0.00001 > ../next_gen_simulation_finalt/$1
-rm population_output.txt

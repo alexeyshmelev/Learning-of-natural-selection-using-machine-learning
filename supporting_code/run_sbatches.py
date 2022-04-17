a033350d804e6a1664d09a570c1546f6ac1ff994
Alexey Shmelev <60858323+alexeyshmelev@users.noreply.github.com>
2022-04-17 18:25:28 +0300

    Add files via upload

--- supporting_code/run_sbatches.py
+++ supporting_code/run_sbatches.py
@@ -5,4 +5,4 @@ files = os.listdir("/".join(os.path.abspath(__file__).split('/')[:-1]))
 
 for file in files:
     print(file)
-    subprocess.call("sbatch " + file)
+    subprocess.call("sbatch " + os.path.abspath(__file__).split('/')[:-1] + "/" + file)
